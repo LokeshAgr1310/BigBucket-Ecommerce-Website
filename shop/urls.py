fbd8858929db8403c6edbd0e999049f23e3e4d50
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-06-26 16:52:08 +0530

    Making Project Layout

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,11 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path("", views.index, name="shopHome"),
+    path("", views.IndexView, name="ShopHome"),
+    path("about/", views.AboutView, name="AboutUs"),
+    path("contact/", views.ContactView, name="ContactUs"),
+    path("tracker/", views.TrackerView, name="TrackingStatus"),
+    path("search/", views.SearchView, name="Search"),
+    path("productView/", views.ProductView, name="ProductView"),
+    path("checkout/", views.CheckoutView, name="Checkout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def IndexView(request):
     return render(request, 'shop/index.html')
+
+def AboutView(request):
+    return HttpResponse("I am at About Page...")
+
+def ContactView(request):
+    return HttpResponse("I am at Contact Page...")
+
+def TrackerView(request):
+    return HttpResponse("I am at Tracker Page...")
+
+def SearchView(request):
+    return HttpResponse("I am at search Page...")
+
+def ProductView(request):
+    return HttpResponse("I am at Product details Page...")
+
+def CheckoutView(request):
+    return HttpResponse("I am at Product checkout Page...")
+
