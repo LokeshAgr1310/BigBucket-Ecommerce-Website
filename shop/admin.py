262eaa2938d9b667a16a707af98f603cf149a35a
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-06-26 17:39:33 +0530

    Creating the admin panel for the Product Model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_desc', 'pub_date')
+    list_filter = ['pub_date']
+
+admin.site.register(Product, ProductAdmin)
