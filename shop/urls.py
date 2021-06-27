ca980c74c9c97ef11156f68bc534da26f3e3969a
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-06-27 22:31:13 +0530

    Changing urls path

--- shop/urls.py
+++ shop/urls.py
@@ -3,11 +3,11 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path("", views.IndexView, name="ShopHome"),
-    path("about/", views.AboutView, name="AboutUs"),
-    path("contact/", views.ContactView, name="ContactUs"),
-    path("tracker/", views.TrackerView, name="TrackingStatus"),
-    path("search/", views.SearchView, name="Search"),
-    path("productView/", views.ProductView, name="ProductView"),
-    path("checkout/", views.CheckoutView, name="Checkout"),
+    path('', views.IndexView, name="ShopHome"),
+    path('about/', views.AboutView, name="AboutUs"),
+    path('contact/', views.ContactView, name="ContactUs"),
+    path('tracker/', views.TrackerView, name="TrackingStatus"),
+    path('search/', views.SearchView, name="Search"),
+    path('productView/', views.ProductView, name="ProductView"),
+    path('checkout/', views.CheckoutView, name="Checkout"),
 ]
