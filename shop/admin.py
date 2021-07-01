88e6bfefa1a3dd005fc820ee4e076765df4bdd19
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-02 00:16:53 +0530

    Change the Admin Default Behaviour

--- shop/admin.py
+++ shop/admin.py
@@ -3,7 +3,7 @@ from .models import Product
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_desc', 'pub_date')
+    list_display = ('product_no','product_name', 'product_desc', 'pub_date')
     list_filter = ['pub_date']
 
 admin.site.register(Product, ProductAdmin)
