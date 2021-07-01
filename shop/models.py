ce7ff3beefd5cd6e0aa9c150d1b5da612d42b55a
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-02 00:17:28 +0530

    Modifying our Shop Models

--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    product_no = models.IntegerField(default=1)
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    sub_category = models.CharField(max_length=50, default="")
     product_desc = models.CharField(max_length=300)
+    price = models.IntegerField(default=0)
     pub_date = models.DateTimeField()
+    product_image = models.ImageField(upload_to="shop/img", default="")
 
     def __str__(self):
         return self.product_name
