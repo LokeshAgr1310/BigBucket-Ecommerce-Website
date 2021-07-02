8f0cf9dafc5d3455bab6a8ac499db8ccb39c38a1
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-03 00:19:24 +0530

    Registering Contact Model

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Contact
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_no','product_name', 'product_desc', 'pub_date')
     list_filter = ['pub_date']
 
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    list_filter = ['pub_date']
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Contact, ContactAdmin)
