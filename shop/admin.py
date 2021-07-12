15df4fdadaf98037f5ee61d24223cb856ad0644f
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-12 15:17:29 +0530

    Connecting database to the Admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Contact
+from .models import Product, Contact, Order, OrderUpdate
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -10,5 +10,14 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone')
     list_filter = ['pub_date']
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customerName', 'customerPhone', 'orderDate')
+    list_filter = ['orderDate']
+
+class OrderUpdateAdmin(admin.ModelAdmin):
+    list_filter = ['updateTimeStamp']
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderUpdate, OrderUpdateAdmin)
