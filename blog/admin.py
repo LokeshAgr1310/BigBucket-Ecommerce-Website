ad9109c0d507f11499b06742f01f3ed928440ba3
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-12 23:22:14 +0530

    Connecting to the Blog Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('blogTitle','heading', 'postDate')
+    list_filter = ['postDate']
+
+admin.site.register(BlogPost, BlogPostAdmin)
