c7591d422e702b4b1924c6923551496a35ba9ffd
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-12 23:23:27 +0530

    Configuring the Blog and BlogPost Views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import BlogPost
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    blogPosts = BlogPost.objects.all()
+    context = {'blogposts': blogPosts}
+    return render(request, 'blog/index.html', context)
+
+def blogPost(request, id):
+    post = BlogPost.objects.get(id=id)
+    context = {}
+    
+    # for next Blog
+    try:
+        nextBlog = list(BlogPost.objects.filter(id=post.id + 1))
+        context['nextBlog'] = nextBlog
+    except:
+        context['nextBlog'] = []
+    
+    # for previous Blog
+    try:
+        prevBlog = list(BlogPost.objects.filter(id=post.id - 1))
+        context['prevBlog'] = prevBlog
+    except:
+        context['prevBlog'] = []
+    context['post'] = post
+    return render(request, 'blog/blogPost.html', context)
 
