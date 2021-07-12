9e4ab446fb4bd73be1be3c01e8187f47f9b03896
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-12 15:15:06 +0530

    Make changes in the templates DIR

--- BigBucket/settings.py
+++ BigBucket/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'BigBucket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['BigBucket/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
