415780672e8f8904b2831b385235648bc70b049d
LokehAgr1310 <lokeshagr131003@gmail.com>
2021-07-15 12:47:39 +0530

    Adding payment handler and payment success view

--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,17 @@ from django.urls import reverse
 from django.contrib import messages
 import json
 from django.http import HttpResponse
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+from time import time
+
+# some required variable
+order = None
+
+# razorpay client
+KEY_ID = 'rzp_test_7YRG7ij89HTfP3'
+KEY_SECRET = 'FoxaBwcTKCO5gGkPf0dVd0Ow'
+client = razorpay.Client(auth=(KEY_ID, KEY_SECRET))
 
 # Create your views here.
 
@@ -90,9 +101,9 @@ def ProductView(request, prodId):
     return render(request, 'shop/productView.html', context)
 
 
-
 def CheckoutView(request):
-
+    context = {}
+    # print(request.POST)
     if request.method == "POST":
         itemsJson = request.POST.get('itemsJson', 'Something went wrong')
         name = request.POST.get('inputName', '')
@@ -102,14 +113,59 @@ def CheckoutView(request):
         state = request.POST.get('inputState', '')
         zip_code = request.POST.get('inputZip', '')
         city = request.POST.get('inputCity', '')
+        amount = int(request.POST.get('amount', ''))
         orderDate = timezone.now()
 
-        order = Order(itemsJson=itemsJson, customerName=name, customerEmail=email, customerPhone=phone, customerAddress=address, customerCity=city, customerState=state, customerZipCode=zip_code, orderDate=orderDate)
+        context['customerName'] = name
+        context['customerEmail'] = email
+        context['customerPhone'] = phone
+
+
+        order_amount = amount*100
+        order_currency = 'INR'
+        order_receipt = f'{int(time())}'
+        notes = {'Shipping address': f"{address}, {city}, {state}({zip_code})"}
+        dataDict = {
+            'amount':order_amount,
+            'currency':order_currency,
+            'receipt':order_receipt,
+            'notes':notes
+        }
+        # print(dataDict)
+        userOrder  = client.order.create(dataDict)
+        # print(userOrder)
+        context['order'] = userOrder
+
+        global order
+        order = Order(itemsJson=itemsJson, customerName=name, customerEmail=email, customerPhone=phone, customerAddress=address, customerCity=city, customerState=state, customerZipCode=zip_code, orderDate=orderDate, amount=amount)
         order.save()
         updateDate = timezone.now()
         orderUpdates = OrderUpdate(order_id=order.id, orderUpdateDesc="The order has been successfully placed...", updateTimeStamp=updateDate)
         orderUpdates.save()
-        return render(request, 'shop/ordercheckout.html', {'ordered': True})
+        return render(request, 'shop/handlePayment.html', context)
 
-    return render(request, 'shop/ordercheckout.html')
+    return render(request, 'shop/ordercheckout.html', context)
 
+def handlePayment(request):
+    print(request)
+    return render(request, 'shop/handlePayment.html')
+
+@csrf_exempt
+def paymentSuccess(request):
+    if request.method == 'POST':
+        razorpay_payment_id = request.POST.get('razorpay_payment_id')
+        razorpay_order_id = request.POST.get('razorpay_order_id')
+        razorpay_signature = request.POST.get('razorpay_signature')
+        paymentDict = {
+            'razorpay_order_id': razorpay_order_id,
+            'razorpay_payment_id': razorpay_payment_id,
+            'razorpay_signature': razorpay_signature
+        }
+        try:
+            client.utility.verify_payment_signature(paymentDict)
+            order.payment_id = razorpay_payment_id
+            order.payment_status = True
+            order.save()
+            return render(request, 'shop/payment_success.html')
+        except:
+            return HttpResponse("Your Payment is Failed. Retry Again")
